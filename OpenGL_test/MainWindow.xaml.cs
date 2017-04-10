0eba1ad322c6e1c895a91d60e5fe594e861de09b
Schalaschaska <mihanhlystow@gmail.com>
2017-04-10 11:21:04 +0300

    test_com_2

--- OpenGL_test/MainWindow.xaml.cs
+++ OpenGL_test/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using SharpGL;
+using SharpGL.SceneGraph;
+
 
 namespace OpenGL_test
 {
@@ -30,46 +32,59 @@ namespace OpenGL_test
         public float R;
         public float G;
         public float B;
-        
-        private void OpenGLControl_OpenGLInitialized(object sender, SharpGL.SceneGraph.OpenGLEventArgs args)
+        private float rotation = 0.0f;
+       
+    
+
+    private void OpenGLControl_OpenGLInitialized(object sender, SharpGL.SceneGraph.OpenGLEventArgs args)
         {
             OpenGL gl = GLcontrol.OpenGL;
             gl.ClearColor(0, 0, 0, 0);
             gl.MatrixMode(OpenGL.GL_PROJECTION);
             gl.LoadIdentity();
-            gl.Perspective(45, (float)GLcontrol.Width / (float)GLcontrol.Height, 0.1, 200);
             gl.Enable(OpenGL.GL_DEPTH_TEST);
-            gl.Enable(OpenGL.GL_COLOR_MATERIAL);
             gl.Enable(OpenGL.GL_LIGHTING);
             gl.Enable(OpenGL.GL_LIGHT0);
+            gl.Light(OpenGL.GL_LIGHT1, OpenGL.GL_POSITION, new float[] { 0.0f, 2f, -30f, 0 });
+            gl.Enable(OpenGL.GL_LIGHT1);
+            gl.Enable(OpenGL.GL_COLOR_MATERIAL);
+            gl.Light(OpenGL.GL_LIGHT1, OpenGL.GL_SPOT_CUTOFF, 180.0f);
+            
+
 
 
         }
         private void OpenGLControl_OpenGLDraw(object sender, SharpGL.SceneGraph.OpenGLEventArgs args)
         {
-                
-                var Gl = args.OpenGL;
-                IntPtr a_prt = Gl.NewQuadric();
-                Gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
-                Gl.ClearColor(R, G, B, 0);
-                Gl.QuadricDrawStyle(a_prt, OpenGL.GLU_FILL);
-                Gl.MatrixMode(OpenGL.GL_MODELVIEW);
-                Gl.LoadIdentity();
-                Gl.PushMatrix();
-                Gl.Translate(0, 0, -6);
-                Gl.Rotate(45, 1, 1, 0);
-                Gl.Color(0, 1, 0);
-                Gl.Sphere(a_prt, 2,32,32);
-                Gl.PopMatrix();
-                Gl.End();
-                Gl.Flush();
+
+            OpenGL gl = GLcontrol.OpenGL;
+            IntPtr quad = gl.NewQuadric();
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+            gl.LoadIdentity();
+            gl.Rotate(rotation, 1.0f, 1.0f, 1.0f);
+            gl.Translate(0.0f, 0.0f, 0.0f);
+            gl.Scale(0.3, 0.3, 0.3);
+            gl.PushMatrix();
+            gl.Color(1.0f, 1.0f, 0.0f);
+            //gl.Sphere(quad, 50, 20, 20);
+            gl.Cylinder(quad, 50, 20, 20, 10,10);
             
+            gl.PopMatrix();
+
+            rotation += 8.0f;
 
 
         }
         private void OpenGLControl_Resized(object sender, SharpGL.SceneGraph.OpenGLEventArgs args)
         {
-
+         
+            OpenGL gl = GLcontrol.OpenGL;
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+            gl.LoadIdentity();
+            gl.Viewport(0, 0, (int)Width, (int)Height);
+            gl.Perspective(90.0f, (double)Width / (double)Height, 1, 200.0);
+            gl.LookAt(0, -1, -30, 0, 0, 0, 0, 1, 0);
+            gl.MatrixMode(OpenGL.GL_MODELVIEW);
         }
 
         private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
@@ -86,8 +101,9 @@ namespace OpenGL_test
         {
             B = Convert.ToSingle(slider2.Value / 255);
         }
-
        
+
+
     }
 
 
